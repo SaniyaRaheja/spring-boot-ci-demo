1cf141dc56e717312be9b25842a5eaf0a486436d
Saniya Raheja <saniyaraheja05@gmail.com>
2026-03-28 18:40:09 +0530

    update

---
+++ helloApp/src/main/java/com/saniya/helloApp/HelloAppController.java
@@ -0,0 +1,14 @@
+package com.saniya.helloApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloAppController {
+
+    @GetMapping("/")
+    public String getHome(){
+        return "Hello from ci/cd pipeline ";
+    }
+
+}
